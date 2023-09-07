ae760b81124f802828c21825c958e6ff9c397fd9
Nick Buser <nick.buser@earnin.com>
2023-09-06 23:26:02 -0700

    finished day 1

---
+++ day1/findPosOfBasementMove.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // read input from text file
+    // for each input increment floor according to ( or )
+    // print floor
+    int floor = 0;
+    FILE *file = fopen("input.txt", "r");
+    if (file == NULL){ 
+        printf("Error Reading File\n"); 
+        return 1; 
+    }
+
+    int pos = 0;
+    char buffer[256];
+    while (fgets(buffer, 256, file) != NULL) {
+        for (size_t i = 0; i< strlen(buffer); i++) {
+            pos ++;
+            if (buffer[i] == '(') {
+                floor++;
+            } else if (buffer[i] == ')') {
+                floor--;
+                if (floor == -1) {
+                    printf("Position of basement move: %u\n", pos);
+                    return 0;
+                }
+            }
+        }
+    }
+    fclose(file);
+    return 0;
+}
